91cf3a944f0fd52b7595e3291cd2432fa39a3bd4
Evanson Mbuthia <kiharambuthia@gmail.com>
2020-04-22 01:53:07 +0300

    .

--- src/components/Country.js
+++ src/components/Country.js
@@ -69,8 +69,9 @@ const Country = ({match, countryState, fetchCountry}) => {
         flexDirection: 'row',
         flexWrap: 'wrap',
         width: '100%',
-        height: '50%',
+        height: '45%',
         marginTop: '10px',
+        marginBottom: '5px',
     }
     
     const titleContent = {

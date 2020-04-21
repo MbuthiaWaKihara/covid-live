30047f06386c7ed1524816ffae075bae2933ad3e
Evanson Mbuthia <kiharambuthia@gmail.com>
2020-04-22 01:43:48 +0300

    Updates

---
+++ precache-manifest.e2644f6bfe7adc7a0cbb91f79284ebe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc28376251a92748d7a9f969e50f1631",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "850546cdd57bc31ec349",
+    "url": "/react-tutorial/static/css/2.4cac4a78.chunk.css"
+  },
+  {
+    "revision": "850546cdd57bc31ec349",
+    "url": "/react-tutorial/static/js/2.4390c121.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-tutorial/static/js/2.4390c121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900e750eaf3862751234",
+    "url": "/react-tutorial/static/js/main.5b5652c7.chunk.js"
+  },
+  {
+    "revision": "df9b1cc869f3e7dff733",
+    "url": "/react-tutorial/static/js/runtime-main.b13738ed.js"
+  },
+  {
+    "revision": "263dad672dd27fb2b588e9690a14a19a",
+    "url": "/react-tutorial/static/media/world.263dad67.gif"
+  }
+]);
